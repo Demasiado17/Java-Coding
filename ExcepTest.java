fec0342b091fef125ebe8ea38ad952442865cf72
Johni Martinez <80790359+Demasiado17@users.noreply.github.com>
2024-12-08 13:20:28 -0500

    Update ExcepTest.java

--- ExcepTest.java
+++ ExcepTest.java
@@ -11,5 +11,18 @@ public class ExcepTest {
          System.out.println("Exception thrown  :" + e);
       }
       System.out.println("Out of the block");
+
+      int a[] = new int[2]; // Size 2
+      try {
+        System.out.println("Access element three :" + a[3]); // Accessing 3rd element
+     } catch (ArrayIndexOutOfBoundsException e) {
+        System.out.println("Exception thrown  :" + e);
+     }finally { // Always executed no matter what!
+        a[0] = 6;
+        System.out.println("First element value: " + a[0]);
+        System.out.println("The finally statement is executed");
+     }
+
+
    }
 }
